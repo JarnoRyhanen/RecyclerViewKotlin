38b7afa3ed942d623ba127b9dd47e57230265eaf
Jarno02 <jarno.ryhanen02@gmail.com>
2021-11-03 13:56:01 +0200

    Added kotlin viewbinding

--- app/src/main/java/com/home/recyclerviewkotlin/MainActivity.kt
+++ app/src/main/java/com/home/recyclerviewkotlin/MainActivity.kt
@@ -5,22 +5,23 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.home.recyclerviewkotlin.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity(), RecyclerViewAdapter.OnItemClickListener {
     private val list = generateList(200)
     private val adapter = RecyclerViewAdapter(list, this)
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
-
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.setHasFixedSize(true)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.setHasFixedSize(true)
     }
 
     fun removeItem(view: View) {
